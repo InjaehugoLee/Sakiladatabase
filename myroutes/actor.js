f5c0eb31d5a7d75180ba8b7ffda828cd1b784141
InjaehugoLee <lij5109@gmail.com>
2021-02-08 15:27:30 +0900

    second commit

--- myroutes/actor.js
+++ myroutes/actor.js
@@ -5,6 +5,13 @@ const {Op}= require('sequelize');
 
 // 쿼리 날리는 법
 // http://localhost:4000/actor?actor_id=5&fields=actor_id,first_name,last_name
+
+Actor.create({
+    first_name:'Ingnacio',
+    last_name:'Rodriguez',
+
+})
+
 router.get('/', async(req, res)=>{
     const q = req.query.actor_id;
     const fields = req.query.fields.split(',');
